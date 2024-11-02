336de1d3c78331b7dfdb47b4544c1c590f75eac4
sidith <58284168+sidith@users.noreply.github.com>
2024-11-02 13:46:18 +0900

    added delta time and clock

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pg.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pg.event.get():
@@ -15,6 +18,8 @@ def main():
 
         screen.fill((100, 100, 100))
         pg.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
